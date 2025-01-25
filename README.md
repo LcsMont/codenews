a7cc7869a08e8fc0bb4a205592cc971f647a8def
LcsMont <lucasmmnm@gmail.com>
2025-01-24 22:00:58 -0300

    add scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # codenews
+
 An open space for coders and tech enthusiasts to merge their knowledge and experiences
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,25 @@
-import React from 'react';
+import React from "react";
 
 function Home() {
-    const styles = {
-        container: {
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '100vh',
-            backgroundColor: '#f0f0f0',
-        },
-        heading: {
-            fontSize: '2rem',
-            color: '#333',
-        },
-    };
+  const styles = {
+    container: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      backgroundColor: "#f0f0f0",
+    },
+    heading: {
+      fontSize: "2rem",
+      color: "#333",
+    },
+  };
 
-    return (
-        <div style={styles.container}>
-            <h1 style={styles.heading}>Aloysio! Aloysio!!!</h1>
-        </div>
-    );
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.heading}>Aloysio! Aloysio!!!</h1>
+    </div>
+  );
 }
 
 export default Home;
